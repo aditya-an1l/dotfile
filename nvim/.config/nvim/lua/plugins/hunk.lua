0c2d1b3031c65e1a4c8afd4ef22f6adc13fea800
aditya-an1l <aditya.anil.productions@gmail.com>
2026-07-09 09:16:48 +0530

    Add hunk plugin

--- nvim/.config/nvim/lua/plugins/hunk.lua
+++ nvim/.config/nvim/lua/plugins/hunk.lua
@@ -1,7 +1,71 @@
 return {
   "julienvincent/hunk.nvim",
   cmd = { "DiffEditor" },
+  dependencies = {
+    "MunifTanjir/nui.nvim",
+    -- optional:
+    -- "nvim-tree/nvim-web-devicons",
+    -- "echasnovski/mini.icons",
+  },
   config = function()
-    require("hunk").setup()
+    local hunk = require("hunk")
+    hunk.setup({
+      keys = {
+        global = {
+          quit = { "q" },
+          accept = { "<leader><Cr>" },
+          focus_tree = { "<leader>e" },
+        },
+
+        tree = {
+          expand_node = { "l", "<Right>" },
+          collapse_node = { "h", "<Left>" },
+          open_file = { "<Cr>" },
+          toggle_file = { "a" },
+        },
+
+        diff = {
+          toggle_hunk = { "A" },
+          toggle_line = { "a" },
+          toggle_line_pair = { "s" },
+          prev_hunk = { "[h" },
+          next_hunk = { "]h" },
+          toggle_focus = { "<Tab>" },
+        },
+      },
+
+      ui = {
+        tree = {
+          mode = "nested",
+          width = 35,
+          use_float = false,
+          float = {
+            height = 1.0,
+            border = nil,
+            position = "left",
+            padding = { left = 1, right = 1 },
+            close = { "<Esc>" },
+          },
+        },
+        layout = "vertical",
+        confirm_before_quit = false,
+      },
+
+      icons = {
+        enable_file_icons = true,
+        selected = "󰡖",
+        deselected = "",
+        partially_selected = "󰛲",
+        folder_open = "",
+        folder_closed = "",
+        expanded = "",
+        collapsed = "",
+      },
+
+      hooks = {
+        on_tree_mount = function(_context) end,
+        on_diff_mount = function(_context) end,
+      },
+    })
   end,
 }
